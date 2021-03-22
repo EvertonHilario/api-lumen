2d009eb08e4d168f3af562b8d991539e512b6561
evertonhilario <evertonjuru@gmail.com>
2021-03-22 10:48:26 -0300

    feat: infrastructure abstraction created

---
+++ app/Infrastructure/Eloquent/Users.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Infrastructure\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email',
+    ];
+}
