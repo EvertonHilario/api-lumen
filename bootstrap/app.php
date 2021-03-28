f3d937730062d86d7256638800dfc0ef1b165dfd
evertonhilario <evertonjuru@gmail.com>
2021-03-28 15:33:27 -0300

    feat: enabled facades and registered RepositoryServiceProvider

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
     dirname(__DIR__)
 );
 
-// $app->withFacades();
+$app->withFacades();
 
 $app->withEloquent();
 
@@ -94,6 +94,7 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\RepositoryServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
