975fc1431be8e958e646ca3489c73e467c918d1f
evertonhilario <evertonjuru@gmail.com>
2021-03-28 15:40:25 -0300

    feat: defined user context Domain

---
+++ app/Domain/Models/Users.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Domain\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Users extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'telephone', 'cpf', 'obs'
+    ];
+}
